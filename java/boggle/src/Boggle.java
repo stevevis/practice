205b2c2c28adb92970ad08d502b47a8a74666520
Steve Viselli <steve.viselli@gmail.com>
2014-11-13 20:11:20 +1100

    Implement boggle using DFS

--- java/boggle/src/Boggle.java
+++ java/boggle/src/Boggle.java
@@ -1,6 +1,8 @@
 import com.stevevis.dictionary.DictionaryFactory;
 import com.stevevis.dictionary.IDictionary;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -11,11 +13,11 @@ public class Boggle {
 
     private IDictionary dictionary;
     private char[][] board = new char[BOARD_SIZE][BOARD_SIZE];
+    private List<String> words;
 
     public Boggle() {
         dictionary = DictionaryFactory.buildDictionary();
         initBoard();
-        printWords();
     }
 
     private void initBoard() {
@@ -27,11 +29,60 @@ public class Boggle {
         }
     }
 
-    private void printWords() {
-        // TODO Play the game!
+    public void printBoard() {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                System.out.print(board[i][j]);
+                System.out.print(' ');
+            }
+            System.out.print('\n');
+        }
+    }
+
+    public void printWords() {
+        words = new ArrayList<String>();
+
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                boolean[][] visited = new boolean[BOARD_SIZE][BOARD_SIZE];
+                visited[i][j] = true;
+                dfs(i, j, "", visited);
+            }
+        }
+
+        System.out.println("Found " + words.size() + " words");
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+
+    /*
+     * Perform a depth first search of the boggle board starting at the given location, adding dictionary words to our
+     * list as we find them.
+     */
+    private void dfs(int x, int y, String current, boolean[][] visited) {
+        String word = current + String.valueOf(board[x][y]);
+
+        if (word.length() > 2 && dictionary.contains(word)) {
+            words.add(word);
+        }
+
+        if (dictionary.isPrefix(word)) {
+            for (int i = (x - 1 >= 0 ? x - 1 : 0); i <= (x + 1 < BOARD_SIZE ? x + 1 : x); i++) {
+                for (int j = (y - 1 >= 0 ? y - 1 : 0); j <= (y + 1 < BOARD_SIZE ? y + 1 : y); j++) {
+                    if (!visited[i][j]) {
+                        visited[i][j] = true;
+                        dfs(i, j, word, visited);
+                        visited[i][j] = false;
+                    }
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
         Boggle boggle = new Boggle();
+        boggle.printBoard();
+        boggle.printWords();
     }
 }
